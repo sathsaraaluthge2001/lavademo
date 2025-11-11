bce52711b3ed9252ae845c22ec94f5c95c52fe8a
sathsaraaluthge2001 <sathsaraaluthge2001@example.com>
2025-11-11 22:50:28 +0530

    add logger to LavaApplication.class

--- src/main/java/com/lava/lava/LavaApplication.java
+++ src/main/java/com/lava/lava/LavaApplication.java
@@ -2,12 +2,16 @@ package com.lava.lava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class LavaApplication {
-
+    private static final Logger logger = LogManager.getLogger(LavaApplication.class);
+    
 	public static void main(String[] args) {
 		SpringApplication.run(LavaApplication.class, args);
+        logger.info("🚀 Lava Payroll Management System started successfully!");
 	}
 
 }
